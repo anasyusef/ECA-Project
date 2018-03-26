6d50a3037ca6e10f8989cc39ac5995d79364ed24
Anas Yousef <termosad@hotmail.com>
2018-03-26 11:45:34 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
 
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'AADNkSkgPHIrv0aZt3bsUtm6kPkxybIA0NDY0JsA0nT0FEfYmxuscCEkRK7J7pNV')
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:12345678@localhost/ECAPROJECT'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'SOMETHING-VERY-DIFFICULT')
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:pass@host/Database'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', 25))
     MAIL_USE_TLS = bool(os.environ.get('MAIL_USE_TLS', 1))
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'samenza2807@gmail.com')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['samenza2807@gmail.com']
+    ADMINS = ['email@email.com']
