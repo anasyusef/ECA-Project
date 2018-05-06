48c7507863ad1f7dcb65255275fd45e373faf026
anasyousef1 <termosad@hotmail.com>
2018-05-06 01:40:56 +0100

    Features Added
    Signed-off-by: anasyusef <termosad@hotmail.com>
    Signed-off-by: anasyousef1 <termosad@hotmail.com>

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY', 'SOMETHING-VERY-DIFFICULT')
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:12345678@localhost/ECAPROJECT'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:12345678@127.0.0.1/ecaproject'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_HOST', 'smtp.gmail.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', 25))
     MAIL_USE_TLS = bool(os.environ.get('MAIL_USE_TLS', 1))
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['samenza2807@gmail.com']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'ecaprojectnea@gmail.com')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'EcaProject2018')
+    ADMINS = ['ecaprojectnea@gmail.com']
