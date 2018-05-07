3982c9bd555e2b9ba5b11f06ef8f5ad8fda6783d
anasyousef1 <termosad@hotmail.com>
2018-05-07 15:16:26 +0100

    Features Added
    Signed-off-by: anasyusef <termosad@hotmail.com>
    Signed-off-by: anasyousef1 <termosad@hotmail.com>

--- ecaproject.py
+++ ecaproject.py
@@ -2,5 +2,9 @@ from app.models import *
 
 @app.shell_context_processor
 def make_shell_context():
+    """
+    The keys on the dictionary can be used on the shell within the app context.
+    :return:
+    """
     return {'db': db, 'User': User, 'Role': Role, 'Eca': Eca, 'Datetime': Datetime, 'Registration': Registration,
             'WaitingList': WaitingList, 'Attendance': Attendance, 'setup_db': setup_db}
